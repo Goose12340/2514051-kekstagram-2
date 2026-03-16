1989be426d21305e35c2fa3c53c06d7ad3f5efb4
Goose12340 <Nikitin99S@gmail.com>
2026-03-16 22:14:39 +0700

    changing 2 functions (isStringLengthValid, isStrPalyndrom)

--- js/function.js
+++ js/function.js
@@ -1,11 +1,23 @@
-function stringLength(str, length) {
-  return str.length <= length;
-}
+const isStringLengthValid = (str, length) => str.length <= length;
 
-function strPalyndrom(randomStr) {
-  const newStr = randomStr.replaceAll(' ', '').toLowerCase();
-  const reverseStr = newStr.split('').reverse().join('');
-  return newStr === reverseStr;
+
+function isStrPalyndrom(str) {
+  const optiStr = str.replaceAll(' ', '').toLowerCase();
+
+  let left = 0;
+  let right = optiStr.length - 1;
+
+  while (left < right) {
+    if (optiStr[left] !== optiStr[right]) {
+      return false;
+    }
+    else {
+      left++;
+      right--;
+      return true;
+    }
+  }
+
 }
 
 
