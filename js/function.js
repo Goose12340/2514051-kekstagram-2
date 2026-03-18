670c2c0cadf0f51d00eaf856454b7a0de79ad787
Goose12340 <Nikitin99S@gmail.com>
2026-03-18 23:24:30 +0700

    functions changed to arrow functions

--- js/function.js
+++ js/function.js
@@ -1,7 +1,7 @@
 const isStrLengthValid = (str, length) => str.length <= length;
 
 
-function isStrPalyndrom(str) {
+const isStrPalyndrom = (str) => {
   const optiStr = str.replaceAll(' ', '').toLowerCase();
 
   for (let i = 0; i < optiStr.length; i++) {
@@ -10,17 +10,19 @@ function isStrPalyndrom(str) {
     }
   }
   return true;
-}
+};
 
 
-function removeNum(str) {
+const removeNum = (str) => {
   const numberStr = String(str).replaceAll(/\D/g, '');
 
   const num = parseInt(numberStr, 10);
 
   return Number.isNaN(num) ? NaN : num;
-}
+};
 
-console.log(isStrLengthValid('ECMAScript 2022'));
-console.log(isStrPalyndrom('топот'));
-console.log(removeNum('1 кефир, 0.5 батона'));
+isStrLengthValid('ECMAScript 2022');
+
+isStrPalyndrom('топот');
+
+removeNum('1 кефир, 0.5 батона');
