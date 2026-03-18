b5aff5aa344d9faac2b264725dea52ed80e8fc21
Goose12340 <Nikitin99S@gmail.com>
2026-03-18 22:20:41 +0700

    refactoring functions

--- js/function.js
+++ js/function.js
@@ -1,23 +1,15 @@
-const isStringLengthValid = (str, length) => str.length <= length;
+const isStrLengthValid = (str, length) => str.length <= length;
 
 
 function isStrPalyndrom(str) {
   const optiStr = str.replaceAll(' ', '').toLowerCase();
 
-  let left = 0;
-  let right = optiStr.length - 1;
-
-  while (left < right) {
-    if (optiStr[left] !== optiStr[right]) {
+  for (let i = 0; i < optiStr.length; i++) {
+    if (optiStr.at(i) !== optiStr.at(optiStr.length - 1 - i)) {
       return false;
     }
-    else {
-      left++;
-      right--;
-      return true;
-    }
   }
-
+  return true;
 }
 
 
@@ -27,6 +19,8 @@ function removeNum(str) {
   const num = parseInt(numberStr, 10);
 
   return Number.isNaN(num) ? NaN : num;
-
 }
 
+console.log(isStrLengthValid('ECMAScript 2022'));
+console.log(isStrPalyndrom('топот'));
+console.log(removeNum('1 кефир, 0.5 батона'));
